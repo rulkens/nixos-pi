cb0112ed6b6a64a3724cea0dfd455422c04e1139
Alexander Rulkens <rulkens@gmail.com>
2026-02-19 13:21:30 +0100

    initial headless-gl setup

---
+++ modules/headless-gl.nix
@@ -0,0 +1,43 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+with lib;
+
+let
+  cfg = config.rpi.headlessGL;
+in
+{
+  options.rpi.headlessGL = {
+    enable = mkEnableOption "headless GL support for Raspberry Pi";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      # Build essentials
+      gcc
+      gnumake
+      pkg-config
+
+      # X11 / GLX dependencies
+      xorg.libX11
+      xorg.libXi
+      xorg.libXext
+      xorg.xorgserver
+      libGLU
+      glew
+
+      # Virtual framebuffer (Xvfb available via xorgserver)
+      xorg.xorgserver
+    ];
+
+    environment.pathsToLink = [
+      "/include"
+      "/lib"
+      "/share"
+    ];
+  };
+}
--- profiles/dev-box.nix
+++ profiles/dev-box.nix
@@ -6,10 +6,12 @@
     ../modules/tailscale.nix
     ../modules/samba.nix
     ../modules/mosquitto.nix
+    ../modules/headless-gl.nix
   ];
 
   rpi.docker.enable = false;
   rpi.tailscale.enable = false;
   rpi.samba.enable = true;
   rpi.mosquitto.enable = true;
+  rpi.headlessGL.enable = true;
 }
